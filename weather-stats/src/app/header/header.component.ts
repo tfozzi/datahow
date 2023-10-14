bf59bb82ddf3ea36d4995a3e12cd201ba5a3c3ee
thomasfozzi <tfozzi@pmeasuring.com>
2023-10-14 18:43:20 +0200

    header  - migration to scss

--- weather-stats/src/app/header/header.component.ts
+++ weather-stats/src/app/header/header.component.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  appName: string;
+
+  constructor() {
+    this.appName = "Weather"
+   }
 
   ngOnInit(): void {
   }
