56e98d4b17b4c2d8c20b74dd50801865018b98c1
thomasfozzi <tfozzi@pmeasuring.com>
2023-10-16 16:43:25 +0200

    precipitation trend - minor issues

--- weather-stats/src/app/shared/services/precipitations.service.ts
+++ weather-stats/src/app/shared/services/precipitations.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { CurrentLocationService } from './current-location.service';
+import { HttpClient } from '@angular/common/http';
 
+
+// this URL provides both historical and forecast tempratures (from -7 to +7 days)
+const API_BASE_URL = "https://api.open-meteo.com/v1/forecast?&hourly=precipitation_probability&past_days=7";
 @Injectable({
   providedIn: 'root'
 })
 export class PrecipitationsService {
 
-  constructor() { }
+  private apiUrl: any;
+
+  constructor(
+    private currentLocationService: CurrentLocationService,
+    private http: HttpClient
+  ) { 
+    this.apiUrl = API_BASE_URL + "&latitude=" + this.currentLocationService.latitude + "&longitude=" + this.currentLocationService.longitude;
+  }
+
+  getData(){
+    return this.http.get(this.apiUrl);
+  }
 }
